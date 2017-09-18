8a9260a55e8a145a0c05186e45b39e88cb6c1561
mawielan <mawielan@stud.hs-heilbronn.de>
2017-09-18 02:14:35 +0200

    edited commit and set state functionality for touch devices

--- habits/static/habits/js/comment-habit.js
+++ habits/static/habits/js/comment-habit.js
@@ -90,118 +90,172 @@ function getComments(habit_id) {
 //     }
 //   }
 // });
+$(document).ready(function() {
+  $('.section-icon-bottom-displayComments').on('touchstart', function() {
+    console.log('displayComments is fired!');
+    console.log('isDisplayCommentsModeActive: ' + isDisplayCommentsModeActive);
 
-// Display comments
-function displayComments(id) {
-// $(document).on('click','#section-icon-bottom-displayComments', function(e) {
-  console.log('displayComments is fired!');
-  console.log('isDisplayCommentsModeActive: ' + isDisplayCommentsModeActive);
+    var habit_ID = active_habit_id.habit;
+    var acc_id = 'accordion_' + habit_ID;
 
-  var habit_ID = id;
-  var acc_id = 'accordion_' + habit_ID;
+    for (i = 0; i < sessionStorage.length; i++) {
+      if (sessionStorage.key(i) == acc_id) {
+        console.log('Item in session storage');
+        var csrftoken = getCookie('csrftoken');
 
-  for (i = 0; i < sessionStorage.length; i++) {
-    if (sessionStorage.key(i) == acc_id) {
-      console.log('Item in session storage');
-      var csrftoken = getCookie('csrftoken');
-
-      $.ajax({
-        url : "/overview/habit/comments/", // the endpoint,commonly same url
-        type : "POST", // http method
-        data : { csrfmiddlewaretoken : csrftoken,
-          habit_id : habit_ID
+        $.ajax({
+          url : "/overview/habit/comments/", // the endpoint,commonly same url
+          type : "POST", // http method
+          data : { csrfmiddlewaretoken : csrftoken,
+            habit_id : habit_ID
 
 
-         }, // data sent with the post request
+           }, // data sent with the post request
 
-        // handle a successful response
-        success : function(json) {
-          console.log(json); // another sanity check
-          // Create lag time before redirecting
-          var panel = document.getElementById(sessionStorage.getItem(acc_id)).nextElementSibling;
-          panel.style.marginBottom = 1 + "px";
-          var panel2 = panel.nextElementSibling;
-          console.log(panel2);
-          panel2.innerHTML = "";
-          panel2.style.paddingTop = 5 + "px";
+          // handle a successful response
+          success : function(json) {
+            console.log(json); // another sanity check
+            // Create lag time before redirecting
+            var panel = document.getElementById(sessionStorage.getItem(acc_id)).nextElementSibling;
+            panel.style.marginBottom = 1 + "px";
+            var panel2 = panel.nextElementSibling;
+            console.log(panel2);
+            panel2.innerHTML = "";
+            panel2.style.paddingTop = 5 + "px";
 
-          var obj = JSON.parse(json['comments_json']);
-          console.log(obj);
-          var length = Object.keys(obj).length;
+            var obj = JSON.parse(json['comments_json']);
+            console.log(obj);
+            var length = Object.keys(obj).length;
 
-          if (length > 0) {
-            console.log('json[comments_json] has at least one element.');
+            if (length > 0) {
+              console.log('json[comments_json] has at least one element.');
 
-            var tableString = "<table class='table  '>";
-            // div = document.createElement('div');
-            tableString += "<thead>";
-            tableString += "<tr>";
-            tableString += "<th>#</th>";
-            tableString += "<th>Date</th>";
-            tableString += "<th>Comment</th>";
-            tableString += "</tr>";
-            tableString += "</thead>";
-
-            tableString += "<tbody>";
-
-            for (i = 0; i < length; i++) {
+              var tableString = "<table class='table  '>";
+              // div = document.createElement('div');
+              tableString += "<thead>";
               tableString += "<tr>";
-              tableString += "<td>" + (i+1) + "</td>";
-              tableString += "<td>" + obj[i].fields.created_at + "</td>";
-              tableString += "<td>" + obj[i].fields.comment + "</td>";
+              tableString += "<th>#</th>";
+              tableString += "<th>Date</th>";
+              tableString += "<th>Comment</th>";
               tableString += "</tr>";
+              tableString += "</thead>";
+
+              tableString += "<tbody>";
+
+              for (i = 0; i < length; i++) {
+                tableString += "<tr>";
+                tableString += "<td>" + (i+1) + "</td>";
+                tableString += "<td>" + obj[i].fields.created_at + "</td>";
+                tableString += "<td>" + obj[i].fields.comment + "</td>";
+                tableString += "</tr>";
+              }
+
+              tableString += "</tbody>";
+              tableString += "</table>";
+              panel2.innerHTML = tableString;
+
+
+            } else {
+              console.log('json[comments_json] has no element.');
+              console.log(document.getElementById('h4-habit-title_' + habit_ID));
+              var title = document.getElementById('h4-habit-title_' + habit_ID).innerHTML;
+              console.log(title);
+              panel2.style.textAlign = "center";
+              panel2.innerHTML = '<br><p>Für das Habit ';
+              panel2.innerHTML += '<';
+              panel2.innerHTML += title;
+              panel2.innerHTML += '> sind keine Kommentare vorhanden.</p>';
             }
 
-            tableString += "</tbody>";
-            tableString += "</table>";
-            panel2.innerHTML = tableString;
+
+            if (isDisplayCommentsModeActive) {
+              panel2.hidden = true;
+              isDisplayCommentsModeActive = false;
+              console.log('DisplayCommentsMode is inactive');
+              $('.accordion').prop('disabled', false);
+
+            } else {
+              panel2.hidden = false;
+              panel2.style.overflowY = "scroll";
+              isDisplayCommentsModeActive = true;
+              isAddCommentModeActive = false;
+              console.log('DisplayCommentsMode is active');
+
+              $('.accordion').prop('disabled', true);
+              document.getElementById(acc_id).disabled = false;
+
+            }
+            console.log('DisplayCommentsMode is ' + isDisplayCommentsModeActive);
 
 
-          } else {
-            console.log('json[comments_json] has no element.');
-            console.log(document.getElementById('h4-habit-title_' + habit_ID));
-            var title = document.getElementById('h4-habit-title_' + habit_ID).innerHTML;
-            console.log(title);
-            panel2.style.textAlign = "center";
-            panel2.innerHTML = '<br><p>Für das Habit ';
-            panel2.innerHTML += '<';
-            panel2.innerHTML += title;
-            panel2.innerHTML += '> sind keine Kommentare vorhanden.</p>';
+
+          },
+
+          // handle a non-successful response
+          error : function(xhr,errmsg,err) {
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
           }
+        });
 
-
-          if (isDisplayCommentsModeActive) {
-            panel2.hidden = true;
-            isDisplayCommentsModeActive = false;
-            console.log('DisplayCommentsMode is inactive');
-            $('.accordion').prop('disabled', false);
-
-          } else {
-            panel2.hidden = false;
-            panel2.style.overflowY = "scroll";
-            isDisplayCommentsModeActive = true;
-            isAddCommentModeActive = false;
-            console.log('DisplayCommentsMode is active');
-
-            $('.accordion').prop('disabled', true);
-            document.getElementById(acc_id).disabled = false;
-
-          }
-          console.log('DisplayCommentsMode is ' + isDisplayCommentsModeActive);
-
-
-
-        },
-
-        // handle a non-successful response
-        error : function(xhr,errmsg,err) {
-          console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
-        }
-      });
-
+      }
     }
-  }
-}
+  })
+  $('.section-icon-bottom-addComment').on('touchstart', function() {
+    console.log('addComment is fired!');
+    console.log('isAddCommentModeActive: ' + isAddCommentModeActive);
+    var habit_ID = active_habit_id.habit;
+    var acc_id = 'accordion_' + habit_ID;
+
+    if (sessionStorage.length == 0) {
+      console.log("asasasasass" );
+    }
+
+    for (i = 0; i < sessionStorage.length; i++) {
+      if (sessionStorage.key(i) == acc_id) {
+        console.log('Item in session storage');
+
+        var panel = document.getElementById(sessionStorage.getItem(acc_id)).nextElementSibling;
+        panel.style.marginBottom = 1 + "px";
+        var panel2 = panel.nextElementSibling;
+
+        panel2.innerHTML = '<textarea id="comment-textarea" style="border-color: black; display:block;  margin-top: 5px; margin-left: auto; margin-right: auto;" rows="4" cols="35" placeholder="Write your Comment... "></textarea> ';
+        panel2.innerHTML += '<div class="container">';
+        panel2.innerHTML += '  <button id="section-icon-bottom" type="button" class="btn btn-default" aria-label="Left Align">';
+        panel2.innerHTML += '    <i class="fa fa-file-image-o" aria-hidden="true"></i>';
+        panel2.innerHTML += '  </button>';
+        panel2.innerHTML += '  <button id="section-icon-bottom" type="button" class="btn btn-default" aria-label="Left Align">';
+        panel2.innerHTML += '    <i class="fa fa-file-text-o" aria-hidden="true"></i>';
+        panel2.innerHTML += '  </button>';
+        panel2.innerHTML += '  <button id="section-icon-bottom" type="button" class="btn btn-default" aria-label="Left Align">';
+        panel2.innerHTML += '    <i class="fa fa-bar-chart" aria-hidden="true"></i>';
+        panel2.innerHTML += '  </button>';
+        panel2.innerHTML += '  <button id="section-icon-bottom" type="button" class="btn btn-default" aria-label="Left Align">';
+        panel2.innerHTML += '    <i class="fa fa-map-marker" aria-hidden="true"></i>';
+        panel2.innerHTML += '  </button>';
+        panel2.innerHTML += '  <button id="post-comment-btn" class="btn btn-default" >Post</button>';
+        panel2.innerHTML += '</div>';
+
+        if (isAddCommentModeActive) {
+          panel2.hidden = true;
+          console.log('AddComentMode is inactive');
+          isAddCommentModeActive = false;
+          $('.accordion').prop('disabled', false);
+        } else {
+          panel2.style.minHeight = 145 + "px";
+          panel2.style.paddingTop = 0 + "px";
+          panel2.style.paddingBottom= 0 + "px";
+          panel2.hidden = false;
+          isAddCommentModeActive = true;
+          console.log('AddCommentMode is active');
+          isDisplayCommentsModeActive = false;
+          $('.accordion').prop('disabled', true);
+        }
+
+        console.log('AddComentMode is ' + isAddCommentModeActive);
+      }
+    }
+  });
+})
 
 //Post comment
 $(document).on('click', '#post-comment-btn', function(e) {
